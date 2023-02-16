ead7e0e9fc6e573ed51ba57b5be0cee8786c93f9
showandgo <65027198+showandgo@users.noreply.github.com>
2023-02-15 15:05:07 -1000

    update plot function

--- shiny apps/plot_shp.R
+++ shiny apps/plot_shp.R
@@ -1,12 +1,12 @@
 # Plot vector shapefile, but only Island of interest
-vector <- st_read("data/Moku_NEW.shp")
+vector <- st_read("data/moku_final.shp")
 vector <- st_make_valid(vector)
 cols <- carto.pal(pal1 = "red.pal", n1 = 4, pal2 = "green.pal", n2 = 4, middle = TRUE)
 
 plot_sub <- function(island, lc){
   
   sp_subdf <- vector %>% filter(ISLAND == island) %>% select(NAME2, lc) 
-  subdf <- as.data.frame(sp_subdf)
+#subdf <- as.data.frame(sp_subdf)
   plot(sp_subdf)
   choroLayer(spdf = sp_subdf, 
              breaks = c(-100,-40,-20,-5,0,5,20, 100, 4000),
@@ -22,9 +22,14 @@ plot_sub <- function(island, lc){
   labelLayer(spdf = sp_subdf, 
              txt = "NAME2",
              halo = TRUE)
-  layoutLayer(title = "",
-              author = "Author: Louis Chua", sources = "Source: USGS LCMAP", north = FALSE,
-              frame = FALSE, col = NA, scale = NULL, coltitle = "black")
+  layoutLayer(title = "% Land Cover Change from 2000-2020", 
+              coltitle = "black",
+              postitle = "left", 
+              tabtitle = TRUE,
+              theme = "taupe.pal",
+              author = "Author: Louis Chua", 
+              sources = "Source: USGS LCMAP", 
+              north = FALSE, frame = FALSE, col = NA, scale = NULL)
   north(pos = "topleft")
               
   
